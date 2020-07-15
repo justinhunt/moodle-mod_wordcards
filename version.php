2627999467d5c4f661f3805ea362b47004d46d53
justinhunt <bitwalkerjapan@gmail.com>
2020-07-15 11:44:09 +0900

    versin bump and addition of readme file s Please enter the commit message for your changes. Lines starting

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_wordcards';
-$plugin->version = 2020070500;
+$plugin->version = 2020071500;
 $plugin->requires = 2015111603;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.2.0 (Build 2020070500)";
+$plugin->release = "1.2.1 (Build 2020071500)";
