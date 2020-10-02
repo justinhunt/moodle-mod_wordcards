a39a44da534cf14e990030e78340aa8f1827c3f8
justinhunt <bitwalkerjapan@gmail.com>
2020-10-02 12:55:14 +0900

    version bump

--- version.php
+++ version.php
@@ -12,4 +12,4 @@ $plugin->component = 'mod_wordcards';
 $plugin->version = 2020100200;
 $plugin->requires = 2015111603;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.2.4 (Build 2020100200)";
+$plugin->release = "1.2.5 (Build 2020100200)";
