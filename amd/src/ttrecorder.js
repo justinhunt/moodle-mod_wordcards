8b28d6113ba1fb62e5e09f216315908b1bd25510
justinhunt <bitwalkerjapan@gmail.com>
2021-05-29 23:02:19 +0900

    tweaks to detect Brave and Edge

--- amd/src/ttrecorder.js
+++ amd/src/ttrecorder.js
@@ -81,7 +81,9 @@ define(['jquery', 'core/log', 'mod_wordcards/ttaudiohelper', 'core/notification'
                             notification.alert("Error",'No microphone detected!', "OK");
                             break;
                         default:
-                            notification.alert("Error", error.name, "OK");
+                            //other errors, like from Edge can fire repeatedly so a notification is not a good idea
+                            //notification.alert("Error", error.name, "OK");
+                            log.debug("Error", error.name);
                     }
                 };
 
