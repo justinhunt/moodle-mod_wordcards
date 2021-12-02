3c5959ee6476d6e1075ee60f64638f70a8af03fc
justinhunt <bitwalkerjapan@gmail.com>
2021-12-02 04:49:49 +0000

    added datatables to reports, and a more robust navigation and a cancel button, and a highest scoring attempt grading option

--- version.php
+++ version.php
@@ -9,8 +9,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_wordcards';
-$plugin->version = 2021112100;
+$plugin->version = 2021120200;
 $plugin->requires = 2015111603;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.3.15 (Build 2021112100)";
+$plugin->release = "1.3.16 (Build 2021120200)";
 
