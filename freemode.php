8f1928d54100d165c69485c27e65d0447b464b74
justinhunt <bitwalkerjapan@gmail.com>
2023-03-30 13:08:54 +0000

    some changes

--- freemode.php
+++ freemode.php
@@ -76,8 +76,7 @@ if($config->enablesetuptab){
     $PAGE->set_pagelayout('course');
 }
 
-//load glide
-//$PAGE->requires->css(new moodle_url('https://cdn.jsdelivr.net/npm/glidejs@2.1.0/dist/css/glide.core.min.css'));
+//load free mode (why seperate CSS?)
 $PAGE->requires->css(new moodle_url('/mod/wordcards/freemode.css'));
 $PAGE->requires->js_call_amd(constants::M_COMPONENT . "/mywords", 'init', []);
 $PAGE->requires->js_call_amd(constants::M_COMPONENT . "/freemode", 'init', []);
