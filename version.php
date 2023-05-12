f1c25f6a73a070b09fb8e396f86bc87b243991e6
justinhunt <bitwalkerjapan@gmail.com>
2023-05-12 09:14:43 +0000

    fix for upgrade script change

--- version.php
+++ version.php
@@ -12,5 +12,5 @@ $plugin->component = 'mod_wordcards';
 $plugin->version = 2023051200;
 $plugin->requires = 2015111603;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.3.45 (Build 2023051200)";
+$plugin->release = "1.3.45 (Build 2023051201)";
 
