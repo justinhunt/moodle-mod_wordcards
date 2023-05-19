4fd558241f4928209ffbadb478103b0c67e280da
justinhunt <bitwalkerjapan@gmail.com>
2023-05-19 03:15:37 +0000

    tweaks to backwards compat of external api

--- classes/external.php
+++ classes/external.php
@@ -6,12 +6,14 @@
  * @author  Frédéric Massart - FMCorz.net
  */
 
+global $CFG;
+require_once($CFG->libdir . '/externallib.php');
 
 use mod_wordcards\utils;
 use mod_wordcards\constants;
-use core_external\external_api;
-use core_external\external_function_parameters;
-use core_external\external_value;
+use external_api;
+use external_function_parameters;
+use external_value;
 
 /**
  * External class.
