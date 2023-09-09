2aa0a1a988e7b9fa0cf4c541d8aac8024881e050
justinhunt <bitwalkerjapan@gmail.com>
2023-09-09 10:26:57 +0000

    version bump

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_wordcards';
-$plugin->version = 2023090301;
+$plugin->version = 2023090900;
 $plugin->requires = 2015111603;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.3.47 (Build 2023090301)";
+$plugin->release = "1.3.48 (Build 2023090900)";
