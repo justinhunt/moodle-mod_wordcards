3d481c1214d4366021e68dcf6cdae6c9ddfe2b04
Justin Hunt <bitwalkerjapan@gmail.com>
2024-05-26 13:08:16 +0900

    version bump

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_wordcards';
-$plugin->version = 2024051600;
+$plugin->version = 2024052600;
 $plugin->requires = 2015111603;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.3.62 (Build 2024051600)";
+$plugin->release = "1.3.63 (Build 2024052600)";
