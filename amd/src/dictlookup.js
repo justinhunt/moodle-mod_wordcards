7cae014d11c11ae73acd807f9c5f15d8aaae6d4b
Justin Hunt <bitwalkerjapan@gmail.com>
2024-11-26 14:05:25 +0900

    improvements to language chooser

--- amd/src/dictlookup.js
+++ amd/src/dictlookup.js
@@ -38,18 +38,19 @@ define(['jquery','core/log','core/ajax','core/templates'], function($,log,ajax,t
             }
         },
 
-        add_result_to_page: function(termdata) {
+        replace_placeholder_with_result: function(placeholder, termdata) {
             var that = this;
             templates.render('mod_wordcards/word_wizard_oneresult', termdata).then(
                 function (html, js) {
-                    that.resultscont.prepend(html);
+                    placeholder.replaceWith(html);
                     templates.runTemplateJS(js);
                 }
             );
         },
 
-        getwords: function (allwords,sourcelang,definitionslang) {
+        getwords: async function (allwords,sourcelang,definitionslang) {
             var that = this;
+            var searchbtn = $(".ww_words_fetch_btn");
 
             //if we have no words, do nothing
             if (allwords.trim() === '') {
@@ -62,6 +63,7 @@ define(['jquery','core/log','core/ajax','core/templates'], function($,log,ajax,t
         //but that was too slow because the server would process them sequentially
         // so now we make a request for each word. It would still work with a single request
             var requests = [];
+            var templatepromises = [];
             var wordarray = allwords.split(',');
             for (var i = 0; i < wordarray.length; i++) {
                 var word = wordarray[i].trim();
@@ -72,20 +74,29 @@ define(['jquery','core/log','core/ajax','core/templates'], function($,log,ajax,t
                         async: true
                     });
                     //add placeholders for each word
-                    var tdata = {'term': word, 'termno': i};
-                    templates.render('mod_wordcards/ww_skeleton',tdata).done(function(html, js) {
-                        that.resultscont.append(html);
-                    }).fail(function(ex) {
-                        log.error(ex);
-                    });
+                    let tdata = {'term': word, 'termno': requests.length-1};
+                    templatepromises.push(templates.render('mod_wordcards/ww_skeleton',tdata));
                 }
             }
+            //first create all the placeholders, we use await in case the ajax requests return faster than the placeholders are created
+            //replace button caption with spinner while we wait
+            var searchbtncontent = searchbtn.html();
+            searchbtn.html('<i class="icon fa fa-spinner fa-spin fa-fw"></i>');
+           await Promise.all(templatepromises).then((results) => {
+                results.forEach((html) => {
+                    that.resultscont.append(html);
+                });
+            });
+           //replace the spinner with the original caption
+            searchbtn.html(searchbtncontent);
         
            // Loop through the requests, send and respond to each 
            for (let reqindex=0; reqindex < requests.length; reqindex++){
                 ajax.call([requests[reqindex]],true)[0].then(response=>{
-                    //remove the skeleton placeholder
-                    $('#mod_wordcards_wwskeleton_'+ reqindex).remove();
+
+                    //fethc the placeholder
+                    log.debug('placeholder - #mod_wordcards_wwskeleton_'+ reqindex);
+                    var placeholder = $('#mod_wordcards_wwskeleton_'+ reqindex);
 
                     //if return code=0, disaster, log and continue
                     if (response.success === 0) {
@@ -95,14 +106,14 @@ define(['jquery','core/log','core/ajax','core/templates'], function($,log,ajax,t
                     for (var i = 0; i < terms.length; i++) {
                         var theterm = terms[i];
                         //if a word search failed
-                        if (theterm.count === 0) {
+                        if (theterm.count === 0 || theterm.results.length===0) {
                             var senses = [];
                             senses.push({
-                                definition: '', sourcedefinition: 'No def. available',
+                                definition: 'No definition', sourcedefinition: 'No definition',
                                 modelsentence: '', senseindex: 0, translations: '{}'
                             })
                             var tdata = {term: theterm.term, senses: senses, modid: that.modid};
-                            allterms_result.push(tdata);
+                            that.replace_placeholder_with_result(placeholder,tdata);
 
                         } else {
                             var tdata = {term: theterm.term, senses: [], modid: that.modid};
@@ -123,8 +134,14 @@ define(['jquery','core/log','core/ajax','core/templates'], function($,log,ajax,t
                                 if (definitionslang !== "en") {
                                     if (sense.hasOwnProperty('lang_' + definitionslang)) {
                                         definition = sense['lang_' + definitionslang];
-                                    } else if (definitionslang === 'en') {
+                                    } else {
+                                        definition = 'No translation available';
+                                    }
+                                }else{
+                                    if (sense.hasOwnProperty('meaning')) {
                                         definition = sense.meaning;
+                                    } else if (sense.hasOwnProperty('lang_en')) {
+                                        definition = sense['lang_en'];
                                     } else {
                                         definition = 'No translation available';
                                     }
@@ -133,101 +150,16 @@ define(['jquery','core/log','core/ajax','core/templates'], function($,log,ajax,t
                                 //model sentence)
                                 var modelsentence = sense.example;
 
-
                                 tdata.senses.push({
                                     definition: definition, sourcedefinition: sourcedefinition,
                                     modelsentence: modelsentence, senseindex: sindex, translations: translations
                                 });
                             }//end of results loop
-                            
-                            that.add_result_to_page(tdata);
+                            that.replace_placeholder_with_result(placeholder,tdata);
                         }
                     }//end of terms loop
                 });
            }
-           
-           return;
-
-                  
-           var wordpromises = [];
-           for (var reqindex=0; reqindex < requests.length; reqindex++){
-                wordpromises.push(ajax.call([requests[reqindex]],true)[0]); 
-           }
-
-      
-          // This, oddly, did not run the requests in parallel.
-          // So it was too slow and errored on occasion (timeouts I think).
-          //var wordpromises = ajax.call(requests,true);
-         
-            
-            Promise.all(wordpromises).then(function(allresponses){
-                var allterms_result = [];
-                if(allresponses.length===0){
-                    return allterms_result;
-                }
-
-                for(var responseindex = 0; responseindex < allresponses.length; responseindex++) {
-
-                    var response = allresponses[responseindex];
-
-                    //if return code=0, disaster, log and continue
-                    if (response.success === 0) {
-                        log.debug(response.payload);
-                    }
-                    var terms = JSON.parse(response.payload);
-                    for (var i = 0; i < terms.length; i++) {
-                        var theterm = terms[i];
-                        //if a word search failed
-                        if (theterm.count === 0) {
-                            var senses = [];
-                            senses.push({
-                                definition: '', sourcedefinition: 'No def. available',
-                                modelsentence: '', senseindex: 0, translations: '{}'
-                            })
-                            var tdata = {term: theterm.term, senses: senses, modid: that.modid};
-                            allterms_result.push(tdata);
-
-                        } else {
-                            var tdata = {term: theterm.term, senses: [], modid: that.modid};
-                            for (var sindex in theterm.results) {
-                                var sense = theterm.results[sindex];
-                                //by default its term:English def:English
-                                var sourcedefinition = sense.definition;
-                                var alltrans = {};
-                                for (var langkey in sense) {
-                                    if (sense.hasOwnProperty(langkey) && langkey.startsWith('lang_')) {
-                                        alltrans[langkey.substring(5)] = sense[langkey];
-                                    }
-                                }
-
-                                var translations = JSON.stringify(alltrans);
-                                var definition = sourcedefinition;
-                                //if its NOT term:english and def:english, we pull the definition from the translation
-                                if (definitionslang !== "en") {
-                                    if (sense.hasOwnProperty('lang_' + definitionslang)) {
-                                        definition = sense['lang_' + definitionslang];
-                                    } else if (definitionslang === 'en') {
-                                        definition = sense.meaning;
-                                    } else {
-                                        definition = 'No translation available';
-                                    }
-                                }
-
-                                //model sentence)
-                                var modelsentence = sense.example;
-
-
-                                tdata.senses.push({
-                                    definition: definition, sourcedefinition: sourcedefinition,
-                                    modelsentence: modelsentence, senseindex: sindex, translations: translations
-                                });
-                            }//end of results loop
-                            allterms_result.push(tdata);
-                        }
-                    }//end of terms loop
-                }//end of allresponses loop
-                that.update_page(allterms_result );
-            });//end of promise then
     
         },
     }
