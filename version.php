572015109290905386e4513f507b1dabad329b7c
Justin Hunt <bitwalkerjapan@gmail.com>
2026-04-20 10:43:09 +0900

    bumped minimum Moodle version to 4.3

--- version.php
+++ version.php
@@ -10,6 +10,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_wordcards';
 $plugin->version = 2026042000;
-$plugin->requires = 2015111603;
+$plugin->requires = 2023100900; // Requires Moodle 4.3
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = "1.3.87 (Build 2026042000)";
